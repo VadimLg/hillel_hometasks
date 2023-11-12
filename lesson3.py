a8374d5f3c2a91a10fe7f59ed6b1aa4e10b4d3b4
kva <kva.email@gmail.com>
2023-11-12 15:44:57 +0200

    Урок 3, Задача 1, пересдача.

--- lesson3.py
+++ lesson3.py
@@ -1,124 +1,53 @@
 # Задача 1
-grocery_list = []
-message_add = 'Добавьте продукт'
-message_current = 'Сейчас в корзине следующие продукты:\n'
-message_removal = 'Хотите удалить продукт'
-message_order_is_processed = 'Заказ оформлен. До свидания! Спасибо за сотрудничесво!'
-message_incorrect_answer = 'Был введен некорретный ответ, нужно вводить (Y/N)"'
 
-print('Добавьте в корзину 5 продуктов')
+grocery_list = input("Напишіть продукти (5 або більше), які потрібно купити, використовуйте пробіл як роздільник: ").split()
 
-current_product = input(message_add + ' 1: ')
-grocery_list.append(current_product)
+if len(grocery_list) >= 5:
+    # Відображає список доданих продуктів.
+    print(f"Зараз у кошику такі продукти: {str(grocery_list).strip('[]')}")
+    product_all_removal_flag = input('Бажаєте видалити з корзини деякі продукти? (Д/Н):')
+    product_all_removal_flag = product_all_removal_flag.lower()
 
-current_product = input(message_add + ' 2: ')
-grocery_list.append(current_product)
+    if product_all_removal_flag == 'д':
+        # Бажаєте видалити з корзини деякі продукти? - Да
+        # Видалення 1
+        product_number = input(f"Видалення 1. Вкажіть порядковий номер продукту, який потрібно видалити (от 1 до {len(grocery_list)}): ")
+        grocery_list.pop(int(product_number) - 1)
+        print(f"Зараз у кошику такі продукти: {str(grocery_list).strip('[]')}")
 
-current_product = input(message_add + ' 3: ')
-grocery_list.append(current_product)
+        # Видалення 2
+        product_number = input(f"Видалення 2. Вкажіть порядковий номер продукту, який потрібно видалити (от 1 до {len(grocery_list)}): ")
+        grocery_list.pop(int(product_number) - 1)
+        print(f"Зараз у кошику такі продукти: {str(grocery_list).strip('[]')}")
 
-current_product = input(message_add + ' 4: ')
-grocery_list.append(current_product)
+        # Видалення 3
+        product_number = input(f"Видалення 3. Вкажіть порядковий номер продукту, який потрібно видалити (от 1 до {len(grocery_list)}): ")
+        grocery_list.pop(int(product_number) - 1)
+        print(f"Зараз у кошику такі продукти: {str(grocery_list).strip('[]')}")
 
-current_product = input(message_add + ' 5: ')
-grocery_list.append(current_product)
+        # Видалення 4
+        product_number = input(f"Видалення 4. Вкажіть порядковий номер продукту, який потрібно видалити (от 1 до {len(grocery_list)}): ")
+        grocery_list.pop(int(product_number) - 1)
+        print(f"Зараз у кошику такі продукти: {str(grocery_list).strip('[]')}")
 
-# Отображение списка добавленных продуктов.
-print(message_current, '1.', grocery_list[0], '\n2.', grocery_list[1], '\n3.', grocery_list[2], '\n4.', grocery_list[3],
-      '\n5.', grocery_list[4], sep='')
+        # Видалення 5
+        product_number = input(f"Видалення 5. Вкажіть порядковий номер продукту, який потрібно видалити (от 1 до {len(grocery_list)}): ")
+        grocery_list.pop(int(product_number) - 1)
 
-product_all_removal_flag = input('Хотите удалить из корзины некоторые продукты? (Y/N):')
-product_all_removal_flag = product_all_removal_flag.lower()
+        # Перевірка порожній кошик чи ні після видалення.
+        if len(grocery_list) == 0:
+            # Порожній кошик після видалення, додайте продукти.
+            add_list = input("Кошик порожній, додайте продукти, використовуйте пробіл як роздільник: ").split()
+            grocery_list = grocery_list + add_list
+            print(f"У кошику тепер є такі продукти: {str(grocery_list).strip('[]')}\nДо побачення!")
+        else:
+            # Не порожній кошик після видалення, до побачення.
+            print(f"У кошику є такі продукти: {str(grocery_list).strip('[]')}\nДо побачення!")
 
-if product_all_removal_flag == 'y' or product_all_removal_flag == 'yes':
-
-    product_removal_flag = input(message_removal + ' 1? (Y/N)')
-    product_removal_flag = product_removal_flag.lower()
-    if product_removal_flag == 'y' or product_removal_flag == 'yes':
-        grocery_list[0] = ''
-        # Отображение списка после удаления продукта.
-        print(message_current, '1.', grocery_list[0], '\n2.', grocery_list[1], '\n3.', grocery_list[2], '\n4.',
-              grocery_list[3],
-              '\n5.', grocery_list[4], sep='')
-    elif product_removal_flag != 'n' or product_removal_flag == 'no':
-        print(message_incorrect_answer)
-
-    product_removal_flag = input(message_removal + ' 2? (Y/N)')
-    product_removal_flag = product_removal_flag.lower()
-    if product_removal_flag == 'y' or product_removal_flag == 'yes':
-        grocery_list[1] = ''
-        # Отображение списка после удаления продукта.
-        print(message_current, '1.', grocery_list[0], '\n2.', grocery_list[1], '\n3.', grocery_list[2], '\n4.',
-              grocery_list[3],
-              '\n5.', grocery_list[4], sep='')
-    elif product_removal_flag != 'n' or product_removal_flag == 'no':
-        print(message_incorrect_answer)
-
-    product_removal_flag = input(message_removal + ' 3? (Y/N)')
-    product_removal_flag = product_removal_flag.lower()
-    if product_removal_flag == 'y' or product_removal_flag == 'yes':
-        grocery_list[2] = ''
-        # Отображение списка после удаления продукта.
-        print(message_current, '1.', grocery_list[0], '\n2.', grocery_list[1], '\n3.', grocery_list[2], '\n4.',
-              grocery_list[3],
-              '\n5.', grocery_list[4], sep='')
-    elif product_removal_flag != 'n' or product_removal_flag == 'no':
-        print(message_incorrect_answer)
-
-    product_removal_flag = input(message_removal + ' 4? (Y/N)')
-    product_removal_flag = product_removal_flag.lower()
-    if product_removal_flag == 'y' or product_removal_flag == 'yes':
-        grocery_list[3] = ''
-        # Отображение списка после удаления продукта.
-        print(message_current, '1.', grocery_list[0], '\n2.', grocery_list[1], '\n3.', grocery_list[2], '\n4.',
-              grocery_list[3],
-              '\n5.', grocery_list[4], sep='')
-    elif product_removal_flag != 'n' or product_removal_flag == 'no':
-        print(message_incorrect_answer)
-
-    product_removal_flag = input(message_removal + ' 5? (Y/N)')
-    product_removal_flag = product_removal_flag.lower()
-    if product_removal_flag == 'y' or product_removal_flag == 'yes':
-        grocery_list[4] = ''
-        # Отображение списка после удаления продукта.
-        print(message_current, '1.', grocery_list[0], '\n2.', grocery_list[1], '\n3.', grocery_list[2], '\n4.',
-              grocery_list[3],
-              '\n5.', grocery_list[4], sep='')
-    elif product_removal_flag != 'n' or product_removal_flag == 'no':
-        print(message_incorrect_answer)
-
-    # Проверка есть ли продукты в списке после 5 запросов на удаление продуктов.
-    if grocery_list[0] == '' and grocery_list[1] == '' and grocery_list[2] == '' and grocery_list[3] == '' and \
-            grocery_list[4] == '':
-        print('Список пустой, пожалуйста добавьте продукты.')
-
-        current_product = input(message_add + ' 1: ')
-        grocery_list[0] = current_product
-
-        current_product = input(message_add + ' 2: ')
-        grocery_list[1] = current_product
-
-        current_product = input(message_add + ' 3: ')
-        grocery_list[2] = current_product
-
-        current_product = input(message_add + ' 4: ')
-        grocery_list[3] = current_product
-
-        current_product = input(message_add + ' 5: ')
-        grocery_list[4] = current_product
-
-        # Отображение списка снова добавленных продуктов
-        print(message_current, '1.', grocery_list[0], '\n2.', grocery_list[1], '\n3.', grocery_list[2], '\n4.',
-              grocery_list[3],'\n5.', grocery_list[4], sep='')
-        # Отображаем что заказ оформлен (после повторного добавления продуктов если список был пустой)
-        print(message_order_is_processed)
     else:
-        # Отображаем что заказ оформлен (когда список не был пустым после удаления некоторых продуктов)
-        print(message_order_is_processed)
+        # Бажаєте видалити з корзини деякі продукти? - Ні:
+        print(f"У кошику є такі продукти: {str(grocery_list).strip('[]')}\nДо побачення!")
 
-elif product_all_removal_flag == 'n' or product_all_removal_flag == 'no':
-    # Отбражаем когда был введен ответ "N" на вопрос 'Хотите удалить из корзины некоторые продукты? (Y/N):'
-    print(message_order_is_processed)
 else:
-    # Отбражаем когда был введен некорректный ответ на вопрос 'Хотите удалить из корзины некоторые продукты? (Y/N):'
-    print(message_incorrect_answer)
+    # Було додано менше 5 продуктів.
+    print("Додано менше 5 продуктів.\nДо побачення!")
