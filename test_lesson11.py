e01a1c12954c125de5d0ef7262b8f641f89c5c89
kva <kva.email@gmail.com>
2023-12-10 21:50:28 +0200

    Урок 11.

---
+++ test_lesson11.py
@@ -0,0 +1,26 @@
+import time
+
+
+def test_sleep_1():
+    print("Зайнятий 2 секунди")
+    time.sleep(2)
+
+
+def test_sleep_2():
+    print("Зайнятий 2 секунди")
+    time.sleep(2)
+
+
+def test_sleep_3():
+    print("Зайнятий 2 секунди")
+    time.sleep(2)
+
+
+def test_sleep_4():
+    print("Зайнятий 2 секунди")
+    time.sleep(2)
+
+
+def test_sleep_unique():
+    print("Зайнятий 2 секунди")
+    time.sleep(2)
