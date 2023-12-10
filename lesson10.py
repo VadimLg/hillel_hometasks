0b750e05d0ddcdd994b1781917dcc706877e32ba
kva <kva.email@gmail.com>
2023-12-10 18:16:51 +0200

    Урок 10.

---
+++ lesson10.py
@@ -0,0 +1,58 @@
+print("Задача 1")
+# 1) Напишіть 10 тестів(можна що б просто повертали Тру(щоб проходили)) імена тестів повині йти підряд
+# test_1, test_2 ... . Повішайти на них три декоратора old, main, new. кожен декоратор повинен бути на 3 функціях
+# на одній з функцій повино бути повішано два декоратора old i main.
+# додайти їх в python.ini що б були правильні виводи
+#
+print("Тести знаходяться у файлі test_1_lesson10.py")
+#
+print("Прогони:")
+#
+print("1) всі тексти де немає лейби old")
+print('У вікні Terminal виконуємо: pytest -m "not old" -v test_1_lesson10.py')
+print("Скріншот у файлі result_of_run_1.png", end="\n\n")
+#
+print("2) тест де пересікаються old i main")
+print('У вікні Terminal виконуємо: pytest -m "old and main"" -v test_1_lesson10.py')
+print("Скріншот у файлі result_of_run_2.png", end="\n\n")
+#
+print("3) тести з маркерами main, new")
+print('У вікні Terminal виконуємо: pytest -m "main or new"" -v test_1_lesson10.py')
+print("Скріншот у файлі result_of_run_3.png", end="\n\n")
+
+
+print("Задача 2")
+# Візьміть задачу з минулого уроку(
+# 3) зробіть функцію як ми робили з додаванням тільки замість двох чисел зробіть три числа і протестуйте її всіма трьома методами
+# ) модернізуйте її так що кожний раз коли ми її запускаємо те що ми туди передаєм та результат повинно записуватись в файл log.txt
+# Зверніть увагу на те що в файл повинно дозаписуватись, а не затератись.
+# Уявіть що ця функція являється легасі кодом і ви її не можете змінювати,
+# тому потрібно використовувати декоратор. Я хотів би бачити таку реалізацію в домашці три функції:
+# функція з минулого уроку
+# функція що записую текст
+# і декоратор
+
+print('Тест кейси у файлі "test_2_lesson10.py"\n')
+
+def func_decorator(func):
+    def wrapper(*arg, **kwarg):
+        log_text = f"Input data: {str(arg)} "
+        logging_to_file(log_text)
+        result = func(*arg, **kwarg)
+        log_text = f"Result: {str(result)}\n"
+        logging_to_file(log_text)
+        return result
+
+    return wrapper
+
+
+@func_decorator
+def add_three_numbers(number_1: int | float, number_2: int | float, number_3: int | float) -> int | float:
+    "Функція з трьома параметрами"
+    result = number_1 + number_2 + number_3
+    return result
+
+
+def logging_to_file(text_str: str):
+    with open("log.txt", "a") as file:
+        file.write(text_str)
---
+++ test_2_lesson10.py
@@ -0,0 +1,39 @@
+import pytest
+from lesson10 import add_three_numbers
+
+
+# solution 1
+def test_1_add_three_numbers_solution_1():
+    assert add_three_numbers(1, 2, 3) == 6, "positive 1"
+
+
+def test_2_add_three_numbers_solution_1():
+    assert add_three_numbers(3, 2, 1) == 6, "positive 2"
+
+
+def test_3_add_three_numbers_solution_1():
+    assert add_three_numbers(-1, 2, 3) == 6, "failed test 1"
+
+
+def test_4_add_three_numbers_solution_1():
+    assert add_three_numbers(3, 2, -1) == 0, "failed test 2"
+
+
+# solution 2
+@pytest.mark.parametrize("num_1, num_2, num_3, result", [
+    pytest.param(1, 2, 3, 6, id="positive 1"),
+    pytest.param(3, 2, 1, 6, id="positive 2"),
+    pytest.param(-1, 2, 3, 6, id="failed test 1"),
+    pytest.param(3, 2, -1, 0, id="failed test 2")])
+def test_add_three_numbers_solution_2(num_1, num_2, num_3, result):
+    assert add_three_numbers(num_1, num_2, num_3) == result
+
+
+# solution 3
+
+list_test = [(1, 2, 3, 6), (3, 2, 1, 6), (-1, 2, 3, 6), (3, 2, -1, 0)]
+
+
+@pytest.mark.parametrize("num_1, num_2, num_3, result", list_test)
+def test_add_three_numbers_solution_3(num_1, num_2, num_3, result):
+    assert add_three_numbers(num_1, num_2, num_3) == result
