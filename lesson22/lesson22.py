95453c27cb636fd43416e54d703d2f905529505c
kva <kva.email@gmail.com>
2024-02-05 10:15:09 +0200

    Урок 22.

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,21 +1,16 @@
 import requests
 from datetime import datetime
 
-
-def logging_to_file(text_str: str):
-    with open("report_file.txt", "a", encoding="utf-8") as file:
-        file.write(text_str + "\n")
-
-
 response = requests.request(method="GET", url="https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
-sys_datetime = datetime.today().strftime('%d-%m-%Y %H:%M:%S')
+
 if response.status_code != 200:
     print("Виникла помилка при отриманні даних!")
 else:
-    logging_to_file(f"Date: {sys_datetime}")
+    result = f"Date: {datetime.today().strftime('%d-%m-%Y %H:%M:%S')}\n"
     i = 1
     for x in response.json():
-        logging_to_file(f"{i}. {x['txt']} to UAH: {x['rate']}")
+        result += f"{i}. {x['txt']} to UAH: {x['rate']}\n"
         i += 1
-    logging_to_file("-" * 20 + "END" + "-" * 20)
+    with open("report_file.txt", "a", encoding="utf-8") as file:
+        file.write(result + "\n")
     print("Додані дані у файлі report_file.")
